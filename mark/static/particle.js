552d0712b37be595030bba2e0b7ecd0526914202
metromark <mptoledo13@gmail.com>
2021-12-08 13:05:34 +0800

    simulacrum

---
+++ mark/static/particle.js
@@ -0,0 +1,63 @@
+class Particle {
+    constructor() {
+        this.pos = createVector(random(width), random(height));
+        this.vel = createVector(0, 0);
+        this.acc = createVector(0, 0);
+        // this.maxspeed = sliderSpeed.value();
+        this.maxspeed = 10;
+        this.prevPos = this.pos.copy();
+    }
+
+    update() {
+        this.vel.add(this.acc);
+        this.vel.limit(this.maxspeed);
+        this.pos.add(this.vel);
+        this.acc.mult(0);
+        this.maxspeed = 10;
+    }
+
+    follow(vectors) {
+        let x = floor(this.pos.x / scale);
+        let y = floor(this.pos.y / scale);
+        let index = x + y * cols;
+        let force = vectors[index];
+        this.applyForce(force);
+    }
+
+    applyForce(force) {
+        this.acc.add(force);
+    }
+
+    show() {
+        // stroke(sliderR.value(), sliderG.value(), sliderB.value(), 5);
+        // stroke(colorRed, colorGreen, colorBlue, 5);
+        stroke(emotionRGB[0], emotionRGB[1], emotionRGB[2], 5);
+        strokeWeight(2);
+        line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+        this.updatePrev();
+    }
+
+    updatePrev() {
+        this.prevPos.x = this.pos.x;
+        this.prevPos.y = this.pos.y;
+    }
+
+    edges() {
+        if (this.pos.x > width) {
+            this.pos.x = 0;
+            this.updatePrev();
+        }
+        if (this.pos.x < 0) {
+            this.pos.x = width;
+            this.updatePrev();
+        }
+        if (this.pos.y > height) {
+            this.pos.y = 0;
+            this.updatePrev();
+        }
+        if (this.pos.y < 0) {
+            this.pos.y = height;
+            this.updatePrev();
+        }
+    }
+}
